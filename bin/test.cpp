40b5c2cde58e8b8099b9ac22fda6bb1d07469d77
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-03-29 22:53:19 +0800

    Refactor DLL loading in test.cpp to dynamically resolve the path to lib.dll and improve error handling

--- bin/test.cpp
+++ bin/test.cpp
@@ -1,12 +1,29 @@
 #include <windows.h>
 
+#include <cwchar>
 #include <cstdio>
 
 using HelloFn = void (*)();
 using PrintNumFn = void (*)(int);
 
+static bool append_dll_path(wchar_t* path, size_t cap) {
+    wchar_t* slash = std::wcsrchr(path, L'\\');
+    if (!slash) {
+        return false;
+    }
+    slash[1] = L'\0';
+    return wcscat_s(path, cap, L"lib.dll") == 0;
+}
+
 int main() {
-    HMODULE dll = LoadLibraryW(L"bin\\lib.dll");
+    wchar_t path[MAX_PATH];
+    DWORD n = GetModuleFileNameW(nullptr, path, MAX_PATH);
+    if (!n || n >= MAX_PATH || !append_dll_path(path, MAX_PATH)) {
+        std::fprintf(stderr, "Could not resolve path to lib.dll\n");
+        return 1;
+    }
+
+    HMODULE dll = LoadLibraryW(path);
     if (!dll) {
         std::fprintf(stderr, "LoadLibrary failed: %lu\n", GetLastError());
         return 1;
